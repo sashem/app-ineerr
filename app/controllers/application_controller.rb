bae0f3be03b5a65bbb3fb671713a497af7eb8675
unknown <AIGSVE@SVEN-PC.AIGUASOL.com>
2014-11-06 16:01:53 -0300

    asdasd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,29 @@ class ApplicationController < ActionController::Base
   
   after_filter :set_csrf_cookie_for_ng
 
+  protect_from_forgery
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+  
+  # For all responses in this controller, return the CORS access control headers.
+  
+  def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Max-Age'] = "1728000"
+  end
+  
+  def cors_preflight_check
+    if request.method == "OPTIONS"
+      headers['Access-Control-Allow-Origin'] = 'http://localhost'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
