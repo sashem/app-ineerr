e9dbeaddb31238a0a5bdd92783cbed75a34752fc
unknown <AIGSVE@SVEN-PC.AIGUASOL.com>
2014-11-06 14:32:16 -0300

    primera version online

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,13 @@ Bundler.require(*Rails.groups)
 
 module AppIneerr
   class Application < Rails::Application
-    
-     config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.serve_static_assets = false
+    # config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+    #  allow do
+    #    origins '*'
+    #    resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #  end
+    # end
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
